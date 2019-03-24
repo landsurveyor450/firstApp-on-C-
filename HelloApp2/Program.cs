7c60555d129a3d5dc40857abdcc20ecf6a336239
landsurveyor450 <44806389+landsurveyor450@users.noreply.github.com>
2019-03-24 21:09:16 +0300

    Добавьте файлы проекта.

---
+++ HelloApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}");
+            Console.WriteLine("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"your {age}");
+
+        }
+    }
+}
